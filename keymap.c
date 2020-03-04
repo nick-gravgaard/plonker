cebd94ca991cc8b9dc5cb0e9fcf3b7c0b00e0041
Nick Gravgaard <nick@nickgravgaard.com>
2020-03-04 14:02:59 +0000

    Move arrow keys onto base layer

--- keymap.c
+++ keymap.c
@@ -5,8 +5,6 @@
 #include "eeprom.h"
 #include "keymap_uk.h"
 
-#define UNSHIFTED_UK_DQUO KC_2
-
 enum planck_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
   QUOTES
@@ -14,49 +12,44 @@ enum planck_keycodes {
 
 enum planck_layers {
   _BASE,
-  _LOWER,
-  _RAISE,
+  _NUMBERS,
+  _SYMBOLS_AND_NAV,
   _ADJUST,
   _NAV,
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define UNSHIFTED_UK_DQUO KC_2
+
+#define ESCAPE_OR_NUMBERS LT(_NUMBERS, KC_ESCAPE)
+#define TAB_OR_SYMBOLS    LT(_SYMBOLS_AND_NAV, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
-    KC_TAB,             KC_Q,     KC_W,     KC_E,          KC_R,        KC_T,                  KC_Y,     KC_U,        KC_I,         KC_O,         KC_P,         KC_BSPACE,
-    LCTL_T(KC_ESCAPE),  KC_A,     KC_S,     KC_D,          KC_F,        KC_G,                  KC_H,     KC_J,        KC_K,         KC_L,         KC_SCOLON,    QUOTES,
-    KC_LSHIFT,          KC_Z,     KC_X,     KC_C,          KC_V,        KC_B,                  KC_N,     KC_M,        KC_COMMA,     KC_DOT,       KC_SLASH,     KC_ENTER,
-    KC_LCTRL,           KC_LGUI,  KC_LALT,  TG(_NAV),      MO(_LOWER),  LT(_NAV, KC_SPACE),/**/XXXXXXX,  MO(_RAISE),  KC_LBRACKET,  KC_RBRACKET,  KC_MINUS,     KC_EQUAL
+    ESCAPE_OR_NUMBERS,  KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,        KC_Y,     KC_U,         KC_I,         KC_O,     KC_P,       KC_BSPACE,
+    TAB_OR_SYMBOLS,     KC_A,     KC_S,     KC_D,      KC_F,      KC_G,        KC_H,     KC_J,         KC_K,         KC_L,     KC_SCOLON,  QUOTES,
+    KC_LSHIFT,          KC_Z,     KC_X,     KC_C,      KC_V,      KC_B,        KC_N,     KC_M,         KC_COMMA,     KC_DOT,   KC_UP,      KC_ENTER,
+    KC_LCTRL,           KC_LGUI,  KC_LALT,  KC_MINUS,  KC_EQUAL,  KC_SPACE,/**/XXXXXXX,  KC_LBRACKET,  KC_RBRACKET,  KC_LEFT,  KC_DOWN,    KC_RIGHT
   ),
 
-  [_LOWER] = LAYOUT_planck_grid(
-    KC_F1,    KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,       KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,
-    XXXXXXX,  KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        XXXXXXX,
-    XXXXXXX,  LGUI(KC_1),  LGUI(KC_2),  LGUI(KC_3),  LGUI(KC_4),  LGUI(KC_5),  LGUI(KC_6),  LGUI(KC_7),  LGUI(KC_8),  LGUI(KC_9),  LGUI(KC_0),  XXXXXXX,
-    XXXXXXX,  XXXXXXX,     XXXXXXX,     XXXXXXX,     _______,     XXXXXXX,/***/XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX
+  [_NUMBERS] = LAYOUT_planck_grid(
+    _______,     KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,       KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,
+    KC_0,        KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_F12,
+    LGUI(KC_0),  LGUI(KC_1),  LGUI(KC_2),  LGUI(KC_3),  LGUI(KC_4),  LGUI(KC_5),  LGUI(KC_6),  LGUI(KC_7),  LGUI(KC_8),  LGUI(KC_9),  LGUI(KC_0),  XXXXXXX,
+    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,/***/XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX
   ),
 
-  [_RAISE] = LAYOUT_planck_grid(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_EXLM,  XXXXXXX,  UK_TILD,    XXXXXXX,        XXXXXXX,  UK_PIPE,  XXXXXXX,  KC_PERC,          XXXXXXX,
-    XXXXXXX,  KC_AMPR,  KC_ASTR,  KC_DLR,   XXXXXXX,  UK_AT,      KC_NONUS_HASH,  XXXXXXX,  XXXXXXX,  UK_PND,   XXXXXXX,          KC_GRAVE,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_CIRC,  XXXXXXX,  XXXXXXX,    XXXXXXX,        XXXXXXX,  KC_LPRN,  KC_RPRN,  KC_NONUS_BSLASH,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,/**/XXXXXXX,        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,          XXXXXXX
+  [_SYMBOLS_AND_NAV] = LAYOUT_planck_grid(
+    XXXXXXX,  KC_QUESTION,  XXXXXXX,  KC_EXLM,  XXXXXXX,  UK_TILD,    XXXXXXX,        XXXXXXX,  UK_PIPE,   XXXXXXX,          KC_PERC,    KC_DELETE,
+    _______,  KC_AMPR,      KC_ASTR,  KC_DLR,   XXXXXXX,  UK_AT,      KC_NONUS_HASH,  XXXXXXX,  XXXXXXX,   UK_PND,           XXXXXXX,    KC_GRAVE,
+    XXXXXXX,  XXXXXXX,      XXXXXXX,  KC_CIRC,  XXXXXXX,  XXXXXXX,    XXXXXXX,        XXXXXXX,  KC_SLASH,  KC_NONUS_BSLASH,  KC_PGUP,    XXXXXXX,
+    XXXXXXX,  XXXXXXX,      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,/**/XXXXXXX,        KC_LPRN,  KC_RPRN,   KC_HOME,          KC_PGDOWN,  KC_END
   ),
 
   [_ADJUST] = LAYOUT_planck_grid(
-    _______,    _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,
-    KC_DELETE,  _______,  AU_ON,    AU_OFF,   AU_TOG,   _______,    _______,  _______,  _______,  _______,  _______,  RESET,
-    _______,    _______,  MU_ON,    MU_OFF,   MU_TOG,   _______,    _______,  _______,  _______,  _______,  _______,  _______,
-    _______,    _______,  _______,  _______,  _______,  _______,/**/XXXXXXX,  _______,  _______,  _______,  _______,  _______
-  ),
-
-  [_NAV] = LAYOUT_planck_grid(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    KC_PGUP,    KC_BSPACE,  KC_UP,    KC_DELETE,  XXXXXXX,  XXXXXXX,
-    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    KC_PGDOWN,  KC_LEFT,    KC_DOWN,  KC_RIGHT,   XXXXXXX,  XXXXXXX,
-    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,    KC_HOME,    XXXXXXX,  KC_END,     XXXXXXX,  XXXXXXX,
-    _______,  _______,  _______,  _______,  XXXXXXX,  _______,/**/XXXXXXX,    XXXXXXX,    XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX
+    _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  AU_ON,    AU_OFF,   AU_TOG,   _______,    _______,  _______,  _______,  _______,  _______,  RESET,
+    _______,  _______,  MU_ON,    MU_OFF,   MU_TOG,   _______,    _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,/**/XXXXXXX,  _______,  _______,  _______,  _______,  _______
   ),
 
 };
@@ -87,6 +80,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+// define raise/lower layers so template code below works unchanged
+#define _LOWER _NUMBERS
+#define _RAISE _SYMBOLS_AND_NAV
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
 
 #ifdef AUDIO_ENABLE
 bool muse_mode = false;
