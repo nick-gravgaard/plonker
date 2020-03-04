e1dd50698f60ea070e6e247d31aed883689bebba
Nick Gravgaard <nick@nickgravgaard.com>
2020-03-04 13:47:32 +0000

    Stop build.sh copying .git directory

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+LAYOUT_NAME=plonker
+
 # Set QMK_PATH before calling, eg: `QMK_PATH=~/code/qmk_firmware ./build.sh`
-DEST_PATH=${QMK_PATH}/keyboards/planck/keymaps/
+DEST_PATH=${QMK_PATH}/keyboards/planck/keymaps/${LAYOUT_NAME}
+mkdir $DEST_PATH
 
-# Absolute path this script is in
-SCRIPT_PATH=$(dirname `readlink -f $0`)
-
-# Copy this dir to QMK's keymaps directory
-cp -r $SCRIPT_PATH $DEST_PATH
+# Copy files to new dir in QMK's keymaps directory
+cp -r * $DEST_PATH
 
 # cd to qmk_firmware directory
 cd $QMK_PATH
 # Compile a bin file in this directory
-make planck/ez:ng-planck-layout
+make planck/ez:${LAYOUT_NAME}
 cd -
